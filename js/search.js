f020fdd9e5d3563add3b7b9bcb4efb79f996a538
David Potsiadlo <davidpots@gmail.com>
2021-05-06 12:37:14 -0500

    Adds copyright notice up to 25, pdf badge on search for copyright

--- js/search.js
+++ js/search.js
@@ -58,7 +58,7 @@ function runSearch(searchTerm) {
           (lessons[i].hidden_tags.search(new RegExp("\\b" + searchQueryText + "\\b", 'i')) >= 0)
         ) {
 
-        $('#search-results').append('<li class="song-listing"><h3><a href="'+ lessons[i].url +'"><span>'+ lessons[i].title +'</span></a></h3><p>Lesson #'+ lessons[i].slug +' • '+ lessons[i].category +'</p><p class="featured_label" data-patreon-url="' + lessons[i].patreon_url + '">PDF</p></li>');
+        $('#search-results').append('<li class="song-listing"><h3><a href="'+ lessons[i].url +'"><span>'+ lessons[i].title +'</span></a></h3><p>Lesson #'+ lessons[i].slug +' • '+ lessons[i].category +'</p><p class="featured_label" data-pdf-version="' + lessons[i].pdf_version + '" data-patreon-url="' + lessons[i].patreon_url + '">PDF</p></li>');
       }
 
     }
