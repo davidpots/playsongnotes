562c164f289665288bbddf425de853e4bd2b2a33
David Potsiadlo <davidpots@gmail.com>
2021-06-25 08:22:22 -0500

    Fixes pdf badge amateurism

--- js/search.js
+++ js/search.js
@@ -59,9 +59,9 @@ function runSearch(searchTerm) {
         ) {
 
           if ( lessons[i].pdf_version == "musicnotes" ) {
-            pdf_badge = "<p class=\"featured_label\" data-pdf-version=\"' + lessons[i].pdf_version + '\" data-patreon-url=\"' + lessons[i].patreon_url + '\">$ PDF</p>";
+            pdf_badge = "<p class=\"featured_label\" data-pdf-version=\"" + lessons[i].pdf_version + "\" data-patreon-url=\"" + lessons[i].patreon_url + "\">$ PDF</p>";
           } else {
-            pdf_badge = "<p class=\"featured_label\" data-pdf-version=\"' + lessons[i].pdf_version + '\" data-patreon-url=\"' + lessons[i].patreon_url + '\">PDF</p>";
+            pdf_badge = "<p class=\"featured_label\" data-pdf-version=\"" + lessons[i].pdf_version + "\" data-patreon-url=\"" + lessons[i].patreon_url + "\">PDF</p>";
           }
 
         $('#search-results').append('<li class="song-listing"><h3><a href="'+ lessons[i].url +'"><span>'+ lessons[i].title +'</span></a></h3><p>Lesson #'+ lessons[i].slug +' • '+ lessons[i].category +'</p>' + pdf_badge + '</li>');
